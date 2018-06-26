9b5947f816789a26ba7838b954b52dd3c0a65268
YuYEZhou <1182756902@qq.com>
2018-06-26 21:39:03 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe771;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/stylus/varible.styl';
+  .header
+    display :flex
+    line-height :.86rem
+    background-color :$bgColor
+    color :#fff
+    .header-left
+      width :.64rem
+      float :left
+      .back-icon
+        text-align :center
+        font-size :.32rem
+    .header-input
+      flex :1
+      height :.64rem
+      border-radius :.1rem
+      background-color :#fff
+      margin-top :.12rem
+      margin-left :.2rem
+      line-height :.64rem
+      color :#ccc
+      padding-left :.2rem
+    .header-right
+      float :right
+      width :1.24rem
+      text-align :center
+      .arrow-icon
+        font-size :.24rem
+        margin-left :-.04rem
+</style>
