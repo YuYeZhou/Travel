458107a69f17b2b42ef93c56c8905bbc9e666e04
YuYEZhou <1182756902@qq.com>
2018-06-28 15:12:16 +0800

    city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/stylus/varible.styl'
+  .header
+    position :relative
+    line-height :$headerHeight
+    height :$headerHeight
+    background :$bgColor
+    font-size :.32rem
+    color :#fff
+    text-align :center
+    overflow :hidden
+    .header-back
+      position :absolute
+      width :.64rem
+      color :#fff
+      font-size :.32rem
+      top:0
+      left:0
+      text-align :center
+</style>
