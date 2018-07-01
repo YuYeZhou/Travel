9b1025fe120f33cafea9a7699739c76a5543b492
YuYEZhou <1182756902@qq.com>
2018-07-01 17:13:08 +0800

    animation finished

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -6,20 +6,24 @@
         <div class="banner-title">{{sightName}}</div>
         <div class="banner-number">
           <span class="iconfont banner-icon">&#xe692;</span>
-          39
+          {{gallaryImgs.length}}
         </div>
       </div>
     </div>
-    <common-gallery :imgs='gallaryImgs' v-show="showGallery"  @close="handleGalleryClose"></common-gallery>
+    <fade-animation>
+      <common-gallery :imgs='gallaryImgs' v-show="showGallery"  @close="handleGalleryClose"></common-gallery>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallery from 'common/gallery/Gallery'
+import FadeAnimation from 'common/fade/Fade'
 export default {
   name: 'DetailBanner',
   components: {
-    CommonGallery
+    CommonGallery,
+    FadeAnimation
   },
   props: {
     sightName: String,
